ccf9063359a8100889c57aadb08668a094204749
Ridhim-mittal <72308943+Ridhim-mittal@users.noreply.github.com>
2021-01-11 14:29:36 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -4,8 +4,8 @@ var foodStock, database;
 var dogImg, happyDogImg;
 function preload()
 {
-dogImg = loadImage("images/dogImg.png");
-happyDogImg = loadImage("images/dogImg1.png");	
+dogImg = loadImage("dogImg.png");
+happyDogImg = loadImage("dogImg1.png");	
 }
 
 function setup() {
